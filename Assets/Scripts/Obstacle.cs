136e91f4efd3c781ce01403a0322cc201041c9e3
Eryl Kenner <eryl.kenner@nevada.unr.edu>
2019-10-31 00:09:20 -0700

    Zone radius interpolation now only needs to be done if the node is close to being within the radius

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -37,36 +37,38 @@ public class Obstacle : MonoBehaviour
             index++;
         }
 
-        float radius;
         int prevIndex = (index - 1 + RadiusAtEachAngle.Count) % RadiusAtEachAngle.Count;
         int nextIndex = (index + 1) % RadiusAtEachAngle.Count;
-        float diff = relativeAngle - Angles.ElementAt(index);
-        if (diff < 0.0f)
-        {
-            diff += 360.0f;
-        }
-        float range = Angles.ElementAt(nextIndex) - Angles.ElementAt(index);
-        if (range < 0.0f)
-        {
-            range += 360.0f;
-        }
-        if (diff < 0.5f * range)
-        {
-            float t = diff / (0.5f * range);
-            float midRadius = 0.5f * (RadiusAtEachAngle.ElementAt(prevIndex) + RadiusAtEachAngle.ElementAt(index));
-            radius = Mathf.Lerp(midRadius, RadiusAtEachAngle.ElementAt(index), t);
-        }
-        else
-        {
-            float t = diff / (0.5f * range) - 1.0f;
-            float midRadius = 0.5f * (RadiusAtEachAngle.ElementAt(index) + RadiusAtEachAngle.ElementAt(nextIndex));
-            radius = Mathf.Lerp(RadiusAtEachAngle.ElementAt(index), midRadius, t);
-        }
-
         float distance = Vector3.Distance(nodePosition, transform.position);
-        if (distance <= radius)
+        if(distance <= RadiusAtEachAngle.ElementAt(index) || distance <= RadiusAtEachAngle.ElementAt(prevIndex) || distance <= RadiusAtEachAngle.ElementAt(nextIndex))
         {
-            return 100.0f * (1.0f - distance / radius);
+            float angleDiff = relativeAngle - Angles.ElementAt(index);
+            if (angleDiff < 0.0f)
+            {
+                angleDiff += 360.0f;
+            }
+            float angleRange = Angles.ElementAt(nextIndex) - Angles.ElementAt(index);
+            if (angleRange < 0.0f)
+            {
+                angleRange += 360.0f;
+            }
+            float radius;
+            if (angleDiff < 0.5f * angleRange)
+            {
+                float t = angleDiff / (0.5f * angleRange);
+                float midRadius = 0.5f * (RadiusAtEachAngle.ElementAt(prevIndex) + RadiusAtEachAngle.ElementAt(index));
+                radius = Mathf.Lerp(midRadius, RadiusAtEachAngle.ElementAt(index), t);
+            }
+            else
+            {
+                float t = angleDiff / (0.5f * angleRange) - 1.0f;
+                float midRadius = 0.5f * (RadiusAtEachAngle.ElementAt(index) + RadiusAtEachAngle.ElementAt(nextIndex));
+                radius = Mathf.Lerp(RadiusAtEachAngle.ElementAt(index), midRadius, t);
+            }
+            if (distance <= radius)
+            {
+                return 100.0f * (1.0f - distance / radius);
+            }
         }
         return 0.0f;
     }
